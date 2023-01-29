f171ef6b3f88850a0627df9a334cfe52a04df8eb
sua <sua953@gmail.com>
2023-01-29 22:51:44 +0900

    댓글 조회 service, controller

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
 import { CommentsService } from './comments.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('comments')
+@ApiTags('댓글 API')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
+
+  @Get('/best/:id')
+  @ApiOperation({
+    summary: '대표 의견 조회',
+    description: '주제에 대한 대표 의견(베스트 댓글)을 조회한다.',
+  })
+  async findBestComments(@Param('id', new ParseUUIDPipe()) id: string) {
+    const comments = await this.commentsService.findBestComments(id);
+
+    return { comments };
+  }
 }
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Comments } from './entities/comments.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(
+    @InjectRepository(Comments)
+    private repository: Repository<Comments>,
+  ) {}
+
+  async findBestComments(voteSubjectId: string) {
+    const commentA = await this.repository.findOne({
+      where: {
+        select: 'AGREE',
+        voteSubject: voteSubjectId,
+      },
+      order: {
+        likeCount: 'DESC',
+      },
+    });
+
+    const commentB = await this.repository.findOne({
+      where: {
+        select: 'DISAGREE',
+        voteSubject: voteSubjectId,
+      },
+      order: {
+        likeCount: 'DESC',
+      },
+    });
+
+    return { commentA, commentB };
+  }
+}
