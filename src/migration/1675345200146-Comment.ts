bf2fadf52091ebfc78bbc6bef70d1420f6245207
sua <sua953@gmail.com>
2023-02-02 22:46:06 +0900

    feat: comment migration file 생성

---
+++ src/migration/1675345200146-Comment.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class Comment1675345200146 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
