344bb6cda282fb16b6f1113dce553703c6b61a02
hoontae24 <kadr8941@gmail.com>
2023-02-27 21:16:26 +0900

    feat: [SISO-135] 투표 여부 정보 포함

--- src/vote-subjects/vote-subjects.controller.ts
+++ src/vote-subjects/vote-subjects.controller.ts
@@ -6,13 +6,18 @@ import {
   ParseUUIDPipe,
 } from '@nestjs/common';
 import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { VoteVote } from 'src/vote-vote/entities/vote-vote.entity';
+import { VoteVoteService } from '../vote-vote/vote-vote.service';
 import { VoteSubjectsService } from './vote-subjects.service';
 import { VoteSubjectsDto } from './dto/vote-subjects.dto';
 
 @Controller('vote-subjects')
 @ApiTags('투표 주제 API')
 export class VoteSubjectsController {
-  constructor(private readonly voteSubjectService: VoteSubjectsService) {}
+  constructor(
+    private readonly voteSubjectService: VoteSubjectsService,
+    private readonly voteVoteService: VoteVoteService,
+  ) {}
 
   @Get()
   @ApiOperation({
@@ -26,10 +31,26 @@ export class VoteSubjectsController {
     status: 200,
   })
   async findAll() {
+    const userId = 'TEST_02'; // FIXME: 임시 userId 사용
     const voteSubjects = await this.voteSubjectService.findAll();
+    const voteVotes = await this.voteVoteService.findBySubjectsAndUser({
+      subjects: voteSubjects,
+      user: { id: userId },
+    });
+
+    const votesDict = voteVotes.reduce((dict, item) => {
+      if (!dict[item.subjectId]) dict[item.subjectId] = [];
+      dict[item.subjectId].push(item);
+      return dict;
+    }, {} as Record<VoteVote['subjectId'], VoteVote[]>);
+
+    const voteSubjectWithVote = voteSubjects.map((subject) => ({
+      ...subject,
+      hasVote: !!votesDict[subject.id]?.length,
+    }));
 
     return {
-      voteSubjects,
+      voteSubjects: voteSubjectWithVote,
     };
   }
 
@@ -49,13 +70,19 @@ export class VoteSubjectsController {
     status: 200,
   })
   async findById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const userId = 'TEST_02'; // FIXME: 임시 userId 사용
     const voteSubject = await this.voteSubjectService.findById(id);
     if (!voteSubject) {
       throw new NotFoundException(`Not found vote-subject by id "${id}"`);
     }
 
+    const voteVotes = await this.voteVoteService.findBySubjectsAndUser({
+      subjects: [voteSubject],
+      user: { id: userId },
+    });
+
     return {
-      voteSubject,
+      voteSubject: { ...voteSubject, hasVote: !!voteVotes.length },
     };
   }
 }
--- src/vote-subjects/vote-subjects.module.ts
+++ src/vote-subjects/vote-subjects.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VoteSubject } from './entities/vote-subject.entity';
 import { VoteSubjectsService } from './vote-subjects.service';
 import { VoteSubjectsController } from './vote-subjects.controller';
+import { VoteVoteModule } from '../vote-vote/vote-vote.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([VoteSubject])],
+  imports: [
+    TypeOrmModule.forFeature([VoteSubject]),
+    forwardRef(() => VoteVoteModule),
+  ],
   controllers: [VoteSubjectsController],
   providers: [VoteSubjectsService],
   exports: [VoteSubjectsService],
